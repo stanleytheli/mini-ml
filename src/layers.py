022b2c6015cb4fed83b4b1b7f235527209eac73b
stanley <stanleytheli@gmail.com>
2025-02-07 22:54:06 -0800

    Added MaxPool layer

--- src/layers.py
+++ src/layers.py
@@ -13,12 +13,100 @@ class Layer:
     def backprop(self, delta):
         return delta
 
+class MaxPool(Layer):
+    def __init__(self, input_shape, pool_shape=(2,2)):
+        """input_shape = (channels, height, width). 
+        pool_shape = (height, width).
+        Creates a MaxPool layer. """
+        self.input_shape = input_shape
+        self.pool_shape = pool_shape
+
+    def feedforward(self, x):
+        """Given a list of m training examples which have shape (channels, height, width),
+        calculates their MaxPool and returns in the form of a list of m tensors 
+        with shape (channels/stride, height/stride, width)."""
+        # x is a list of m images each of shape(c, h, w)
+        m = len(x)
+
+        C, H, W = self.input_shape
+        pool_h, pool_w = self.pool_shape
+
+        h_steps = H // pool_h
+        w_steps = W // pool_w
+
+        # Is there a better way to keep track of max values for backprop while
+        # avoiding python for loops?
+        pooled = [0] * m
+        self.max_indices = [0] * m
+
+        for i in range(m):
+            pooled_image = np.zeros((C, h_steps, w_steps))
+            max_idxs = np.zeros((C, h_steps, w_steps, 2), dtype=int)
+
+            for h in range(h_steps):
+                for w in range(w_steps):
+                    h_start, h_end = h * pool_h, (h + 1) * pool_h
+                    w_start, w_end = w * pool_w, (w + 1) * pool_w
+                    window = x[i][:, h_start:h_end, w_start:w_end]
+
+                    max_vals = window.max(axis=(1,2))
+                    pooled_image[:, h, w] = max_vals
+
+                    for c in range(C):
+                        max_idx = np.argwhere(window[c] == max_vals[c])[0]
+                        max_idxs[c, h, w] = [h_start + max_idx[0], w_start + max_idx[1]]
+
+            pooled[i] = pooled_image
+            self.max_indices[i] = max_idxs
+        
+        return pooled
+        """
+        pooled = [0] * m
+        for i in range(m):
+            pool_batch = [0] * self.channels
+            for c in range(self.channels):
+                image = x[i][:, :, c]
+                windows = image.reshape(self.rows//self.pool_shape[0], self.pool_shape[0],
+                            self.cols//self.pool_shape[1], self.pool_shape[1])
+                pool_batch[c] = windows.max((1, 3))
+            pooled[i] = np.stack(pool_batch, axis=2)
+        return pooled
+        """
+    
+    def backprop(self, delta):
+        """Given the unscaled error deltas [in the form of a list of m error 
+        deltas which each have shape (channels, height/stride, width/stride)] reshapes 
+        them to be compatible with the layer before the MaxPool."""
+        m = len(delta)
+
+        C, H, W = self.input_shape
+        pool_h, pool_w = self.pool_shape
+
+        h_steps = H // pool_h
+        w_steps = W // pool_w
+
+        deltas_list = [0] * m
+        for i in range(m):
+            reshaped_deltas = np.zeros((C, H, W))
+
+            for h in range(h_steps):
+                for w in range(w_steps):
+                    for c in range(C):
+                        h_idx, w_idx = self.max_indices[i][c, h, w]
+                        reshaped_deltas[c, h_idx, w_idx] = delta[i][c, h, w]
+
+            deltas_list[i] = reshaped_deltas
+        
+        return deltas_list
+
+
 class Flatten(Layer):
     def __init__(self, input_shape):
-        """input_shape = (width, height, channels).
+        """input_shape = (channels, height, width).
+        Or just (height, width) for 2D flattening.
         Creates a flattener layer that flattens lists of m 
-        inputs (width, height, channels) --> a single matrix
-        with shape (width*height*channels, m)"""
+        inputs (channels, height, width) --> a single matrix
+        with shape (channels*height*width, m)"""
 
         # input_shape and output_shape are shapes of the individual vectors,
         # NOT the shapes of the matrices that are the actual outputs
