89bac283eda5c2f657e53c178b19c35765b2f643
stanley <stanleytheli@gmail.com>
2025-02-18 11:12:44 -0800

    Put optimizer classes inside of optimizer factory classes

--- src/utils.py
+++ src/utils.py
@@ -21,17 +21,17 @@ class SGD_optimizer:
         self.m = m
     
     def get_optimizer(self):
-        return _SGD(self.eta, self.m)
+        return self._optimizer(self.eta, self.m)
 
-class _SGD:
-    def __init__(self, eta, m):
-        self.eta = eta
-        self.m = m
-    
-    def fn(self, learnables):
-        """Given a list learnables of ndarrays representing the gradient
-        wrt. the learnable parameters, returns the updates in the same form."""
-        return [-(self.eta/self.m)*nabla for nabla in learnables]
+    class _optimizer:
+        def __init__(self, eta, m):
+            self.eta = eta
+            self.m = m
+        
+        def fn(self, learnables):
+            """Given a list learnables of ndarrays representing the gradient
+            wrt. the learnable parameters, returns the updates in the same form."""
+            return [-(self.eta/self.m)*nabla for nabla in learnables]
 
 class SGD_momentum_optimizer:
     def __init__(self, eta, m, beta):
@@ -41,27 +41,27 @@ class SGD_momentum_optimizer:
         self.beta = beta
 
     def get_optimizer(self):
-        return _SGD_momentum(self.eta, self.m, self.beta)
+        return self._optimizer(self.eta, self.m, self.beta)
 
-class _SGD_momentum:
-    def __init__(self, eta, m, beta):
-        self.eta = eta
-        self.m = m
-        self.beta = beta
+    class _optimizer:
+        def __init__(self, eta, m, beta):
+            self.eta = eta
+            self.m = m
+            self.beta = beta
 
-        self.v = None
+            self.v = None
 
-    def fn(self, learnables):
-        """Given a list learnables of ndarrays representing the gradient
-        wrt. the learnable parameters, returns the updates in the same form."""
-        # Forgot the minus sign and spent a few minutes wondering what 
-        # was wrong with my network. Turns out it was doing gradient ascent!
-        if self.v is not None:
-            self.v = [ - (self.beta*v_i + (self.eta/self.m)*nabla_i) 
-                    for v_i, nabla_i in zip(self.v, learnables)]
-        else:
-            self.v = [-(self.eta/self.m)*nabla for nabla in learnables]
-        return self.v
+        def fn(self, learnables):
+            """Given a list learnables of ndarrays representing the gradient
+            wrt. the learnable parameters, returns the updates in the same form."""
+            # Forgot the minus sign and spent a few minutes wondering what 
+            # was wrong with my network. Turns out it was doing gradient ascent!
+            if self.v is not None:
+                self.v = [ - (self.beta*v_i + (self.eta/self.m)*nabla_i) 
+                        for v_i, nabla_i in zip(self.v, learnables)]
+            else:
+                self.v = [-(self.eta/self.m)*nabla for nabla in learnables]
+            return self.v
 
 class noActivation:
     def fn(self, z):
